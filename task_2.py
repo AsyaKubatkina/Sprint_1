89bfe57a2f2a427140682dd2735621c4628895e2
Asem Kubatkina <asemkubatkina0503@gmail.com>
2025-10-16 20:55:52 +0400

    Fixed task_2

--- task_2.py
+++ task_2.py
@@ -1,16 +1,17 @@
 class Tester:
 
-    def __init__(self, name, deadline):
+    def __init__(self, name):
         self.name = name
-        self.deadline = deadline
+        self.deadline = True
 
-    def work_hard(self):
+    def work_hard(self, deadline):
+        self.deadline = deadline
         if self.deadline:
             print(self.name, 'Что ж, ещё часок поработаю!')
         else:
             print(self.name, 'Можно отдыхать')
 
-tester_1 = Tester('tester_1',False)
-tester_1.work_hard()  # 'tester_1 Можно отдыхать'
-tester_2 = Tester('tester_2',True)
-tester_2.work_hard()   # 'tester_2 Что ж, ещё часок поработаю!'
+tester_1 = Tester(name='tester_1')
+tester_1.work_hard(deadline=False)  # 'tester_1 Можно отдыхать'
+tester_2 = Tester(name='tester_2')
+tester_2.work_hard(deadline=True)   # 'tester_2 Что ж, ещё часок поработаю!'
